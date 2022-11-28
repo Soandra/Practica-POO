73b54114e9fbff0405e2acf1158cc503e86e0729
Soandra <soandradep@unal.edu.co>
2022-11-27 22:03:04 -0500

    Me quiero morir

---
+++ Python2/uiMain/UIPago.py
@@ -0,0 +1,93 @@
+from Python.gestorAplicacion.usuario.cliente import Cliente
+from Python.uiMain.main import Main
+
+
+class UIPago:
+    @classmethod
+    def Pagar(cls, cliente):
+        print("""
+        Seleccione el tipo de pago que desea realizar
+        1. Pagar prestamo
+        2. Pagar multa""")
+        opcionPagos=int(input())
+        print("Ingresa el ID de la cuenta que deseas aplicar el prestamo")
+        Main.traerCuentas()
+        opcion = int(input())
+        if (opcionPagos == 1):
+                if (len(((Cliente.buscarCuenta(opcion)).getPrestamos())) == 0) :
+                    print("Usted no cuenta con prestamos actualmente")
+                    return
+                
+                print("Prestamos Actualmente activos: ")
+                Main.traerPrestamos(opcion)
+                print("elija el ID del prestamo que desea pagar")
+                numeroDePrestamo=int(input()) #id del prestamo
+                print("""
+                1. Pago parcial
+                2. Pago completo""")
+
+                pago = int(input())
+                if (pago == 1) :
+                    print("Cuántas cuotas desea pagar?")
+                    cuotas = int(input()) #numero de cuotas
+                    print("Valor de la transaccion: ")
+                    print((Cliente.buscarPrestamo(opcion,numeroDePrestamo)).getValorCuota()*cuotas)
+                    print("""
+                    Desea realizar el pago?
+                    1.Si
+                    2.No""")
+                    caso = int(input())
+                    if (caso == 1) :
+                        #cuenta, prestamo, cuota
+                        print(cliente.hacerPagoPrestamo(opcion,numeroDePrestamo,cuotas))
+                          
+                elif (pago == 2) :
+                    print("Valor de la transaccion:")
+                    print(Cliente.buscarPrestamo(opcion,numeroDePrestamo).getValorPrestamo()* ((1+Cliente.buscarPrestamo(opcion,numeroDePrestamo).getInteres())))
+                    print("""
+                    Desea realizar el pago?
+                    1.Si
+                    2.No""")
+                    caso = int(input())
+                    if (caso == 1) :
+                    # cuenta, prestamo
+                        print(cliente.hacerPagoPrestamo(opcion,numeroDePrestamo))
+                          
+        elif (opcionPagos == 2):
+            if (((len(Cliente.buscarCuenta(opcion)).getMultas())) == 0) :
+                print("Usted no tiene multas actualmente")
+                return
+                
+            print("Multas Actualmente activas: ")
+            Main.traerMultas(opcion)
+
+            print("Elija el ID de la multa que desea pagar")
+            numeroDeMulta = int(input())
+
+            print("""
+            1.Pago parcial
+            2.Pago completo""")
+            pago = int(input())
+
+            if (pago == 1) :
+                print("Ingrese el valor de la transaccion")
+                valorM = int(input())
+                print(cliente.hacerPagoMulta(opcion,numeroDeMulta,valorM))
+            if (pago == 2) :
+                print("Valor de la transaccion: ")
+                print(Cliente.buscarMulta(opcion,numeroDeMulta).getMonto())
+                print("""
+                Desea realizar el pago?
+                1.Si
+                2.No""")
+                caso = int(input())
+                if (caso == 1) :
+                    mensaje = cliente.hacerPagoMulta(opcion,numeroDeMulta)
+                    print(mensaje)
+                        
+                    
+                
+            
+        
+    
+
---
+++ Python2/uiMain/UIPrestamo.py
@@ -0,0 +1,25 @@
+from main import *
+class UIPrestamo:
+    @classmethod
+    def prestamo(cls, cliente):
+        print("Ingresa el ID de la cuenta que deseas aplicar el prestamo")
+        Main.traerCuentas()
+        opcion = int(input())
+        if opcion >= 0 and opcion < len(Cliente.listaCuentas):
+            # falta validación de si es corriente o no
+            print("Puedes solicitar un prestamo entre 500.000 y 7'000.000 a 24 cuotas\n Ingresa el valor a solicitar")
+            valor = int(input())
+            if valor < 500000 or valor > 7000000:
+                print("Valor fuera del rando permitido")
+            else:
+                print("Elige el tipo de prestamo (universitario,hobbie,libre)")
+                tipoPrestamo = input()
+
+                if tipoPrestamo != "universitario" and tipoPrestamo != "hobbie" and tipoPrestamo != "libre":
+                    print("El tipo de prestamo ingresado no existe")
+                    return
+
+                cliente.solicitarPrestamo(valor, tipoPrestamo, opcion)
+                prestamos = Cliente.listaCuentas.get(opcion).getprestamos() #organizar
+                print(prestamos.get(len(prestamos) - 1).mensajePrestamo())
+
